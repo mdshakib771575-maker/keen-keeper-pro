b2ea35c1411deca012301f29bb2859bd6f67d3cc
Shakib <mdshakib771575@gmail.com>
2026-04-16 22:12:24 -0700

    some modify frend detail fech

--- src/app/[id]/page.jsx
+++ src/app/[id]/page.jsx
@@ -1,13 +1,12 @@
 import FrienDetailCard from '@/components/FrienDetailCard';
 import Image from 'next/image';
-
+import friendData from "../../../public/data.json"
 import React from 'react';
 
 const FriendDetailPage = async ({ params }) => {
     const { id } = await params;
 
-    const res = await fetch('http://localhost:3000/data.json');
-    const friends = await res.json();
+    const friends = friendData;
     //  console.log(friends);
 
     const friend = friends.find(friend => friend.id == id)
@@ -17,9 +16,9 @@ const FriendDetailPage = async ({ params }) => {
 
 
     return (
-     <div>
-        <FrienDetailCard friend={friend}></FrienDetailCard>
-     </div>
+        <div>
+            <FrienDetailCard friend={friend}></FrienDetailCard>
+        </div>
     );
 };
 
